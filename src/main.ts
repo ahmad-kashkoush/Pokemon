9aea53358446ad8221349cf9c3a767c9ba761b19
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2025-10-14 20:17:53 +0300

    chore: configure toast

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,26 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { VueQueryPlugin } from '@tanstack/vue-query'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(VueQueryPlugin)
+app.use(Toast, {
+  timeout: 3000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: false,
+  closeButton: 'button',
+  icon: true,
+  rtl: false,
+})
 
 app.mount('#app')
